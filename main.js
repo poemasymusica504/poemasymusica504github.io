f76dbb9fba299d84b0895319b7e873abf3049d57
davis45368 <122112897+davis45368@users.noreply.github.com>
2023-03-25 16:27:45 -0500

    Revert "contador de likes"
    This reverts commit cfcdcb5f4ce02a5da1fe2de011970676eda6c13f.

--- main.js
+++ main.js
@@ -6,26 +6,3 @@ ScrollReveal().reveal('.showcase');
 ScrollReveal().reveal('.news-cards', { delay: 500});
 ScrollReveal().reveal('.card-banner-one', { delay: 500});
 ScrollReveal().reveal('.card-banner-two', { delay: 500});
-
-/* Contador de likes*/
-const likeBtn = document.querySelector(".like_btn");
-let likeIcon = document.querySelector("#icon");
-let count = document.querySelector("#count")
-
-let clicked = false;
-
-likeBtn.addEventListener("click", () => {
-    if (!clicked) {
-        clicked = true
-        likeIcon.innerHTML = '<i class="fas fa-thumbs-up"></i>'
-    } else {
-        clicked = false;
-        likeIcon.innerHTML =    '<i class="fas fa-thumbs-up"></i>'
-    }
-});
-
-let counter = 0;
-
-function countingClicks() {
-    document.getElementById("counting").innerHTML = ++counter;   
-}
